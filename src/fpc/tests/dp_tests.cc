d9e42d4d2db24aebdd44180b1086c1a57fbd7c98
Ivan Paraskun <ig.paraskun@gmail.com>
2023-11-23 07:24:17 +0700

    tests/decimal->macro

--- src/fpc/tests/dp_tests.cc
+++ src/fpc/tests/dp_tests.cc
@@ -1,94 +1,79 @@
+#include <float.h>
 #include <gtest/gtest.h>
 #include <math.h>
 
-extern "C"
-{
+extern "C" {
 #include <fpc.h>
 }
 
-TEST(Hex2Dec, Nan)
-{
-    ULL n;
+TEST(Hex2Dec, Nan) {
+  ULL n;
 
-    EXPECT_TRUE(isnan(*dp_hex2dec(&(n = 0xFFFFFFFFFFFFFFFF))));
-    EXPECT_TRUE(isnan(*dp_hex2dec(&(n = 0xFFF0000000000001))));
-    EXPECT_TRUE(isnan(*dp_hex2dec(&(n = 0x7FF0000000000001))));
-    EXPECT_TRUE(isnan(*dp_hex2dec(&(n = 0x7FFFFFFFFFFFFFFF))));
+  EXPECT_TRUE(isnan(*dp_hex2dec(&(n = 0xFFFFFFFFFFFFFFFF))));
+  EXPECT_TRUE(isnan(*dp_hex2dec(&(n = 0xFFF0000000000001))));
+  EXPECT_TRUE(isnan(*dp_hex2dec(&(n = 0x7FF0000000000001))));
+  EXPECT_TRUE(isnan(*dp_hex2dec(&(n = 0x7FFFFFFFFFFFFFFF))));
 }
 
-TEST(Hex2Dec, Inf)
-{
-    ULL n;
+TEST(Hex2Dec, Inf) {
+  ULL n;
 
-    EXPECT_TRUE(isinf(*dp_hex2dec(&(n = 0xFFF0000000000000))));
-    EXPECT_TRUE(isinf(*dp_hex2dec(&(n = 0x7FF0000000000000))));
+  EXPECT_TRUE(isinf(*dp_hex2dec(&(n = 0xFFF0000000000000))));
+  EXPECT_TRUE(isinf(*dp_hex2dec(&(n = 0x7FF0000000000000))));
 }
 
-TEST(Hex2Dec, Normalized)
-{
-    ULL n;
+TEST(Hex2Dec, Normalized) {
+  ULL n;
 
-    EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0xFFEFFFFFFFFFFFFF)), -1.797693134862315708e+308);
-    EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x8010000000000000)), -2.225073858507201383e-308);
-    EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x0010000000000000)), 2.225073858507201383e-308);
-    EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x7FEFFFFFFFFFFFFF)), 1.797693134862315708e+308);
+  EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0xFFEFFFFFFFFFFFFF)), -DBL_MAX);
+  EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x8010000000000000)), -DBL_MIN);
+  EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x0010000000000000)), DBL_MIN);
+  EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x7FEFFFFFFFFFFFFF)), DBL_MAX);
 }
 
-TEST(Hex2Dec, Denormalized)
-{
-    ULL n;
+TEST(Hex2Dec, Denormalized) {
+  ULL n;
 
-    EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x800FFFFFFFFFFFFF)), -2.225073858507200889e-308);
-    EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x8000000000000001)), -4.940656458412465441e-324);
-    EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x0000000000000001)), 4.940656458412465441e-324);
-    EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x000FFFFFFFFFFFFF)), 2.225073858507200889e-308);
+  EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x800FFFFFFFFFFFFF)),
+                   -DBL_MIN + DBL_TRUE_MIN);
+  EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x8000000000000001)), -DBL_TRUE_MIN);
+  EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x0000000000000001)), DBL_TRUE_MIN);
+  EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x000FFFFFFFFFFFFF)),
+                   DBL_MIN - DBL_TRUE_MIN);
 }
 
-TEST(Hex2Dec, Zero)
-{
-    ULL n;
+TEST(Hex2Dec, Zero) {
+  ULL n;
 
-    EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x8000000000000000)), 0);
-    EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x0000000000000000)), 0);
+  EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x8000000000000000)), 0);
+  EXPECT_DOUBLE_EQ(*dp_hex2dec(&(n = 0x0000000000000000)), 0);
 }
 
-TEST(Dec2Hex, Inf)
-{
-    double f;
+TEST(Dec2Hex, Normalized) {
+  double f;
 
-    EXPECT_EQ(*dp_dec2hex(&(f = -1.797693134862315708e+309)), 0xFFF0000000000000);
-    EXPECT_EQ(*dp_dec2hex(&(f = 1.797693134862315708e+309)), 0x7FF0000000000000);
+  EXPECT_EQ(*dp_dec2hex(&(f = -DBL_MAX)), 0xFFEFFFFFFFFFFFFF);
+  EXPECT_EQ(*dp_dec2hex(&(f = -DBL_MIN)), 0x8010000000000000);
+  EXPECT_EQ(*dp_dec2hex(&(f = DBL_MIN)), 0x0010000000000000);
+  EXPECT_EQ(*dp_dec2hex(&(f = DBL_MAX)), 0x7FEFFFFFFFFFFFFF);
 }
 
-TEST(Dec2Hex, Normalized)
-{
-    double f;
+TEST(Dec2Hex, Denormalized) {
+  double f;
 
-    EXPECT_EQ(*dp_dec2hex(&(f = -1.797693134862315708e+308)), 0xFFEFFFFFFFFFFFFF);
-    EXPECT_EQ(*dp_dec2hex(&(f = -2.225073858507201383e-308)), 0x8010000000000000);
-    EXPECT_EQ(*dp_dec2hex(&(f = 2.225073858507201383e-308)), 0x0010000000000000);
-    EXPECT_EQ(*dp_dec2hex(&(f = 1.797693134862315708e+308)), 0x7FEFFFFFFFFFFFFF);
+  EXPECT_EQ(*dp_dec2hex(&(f = -DBL_MIN + DBL_TRUE_MIN)), 0x800FFFFFFFFFFFFF);
+  EXPECT_EQ(*dp_dec2hex(&(f = -DBL_TRUE_MIN)), 0x8000000000000001);
+  EXPECT_EQ(*dp_dec2hex(&(f = DBL_TRUE_MIN)), 0x0000000000000001);
+  EXPECT_EQ(*dp_dec2hex(&(f = DBL_MIN - DBL_TRUE_MIN)), 0x000FFFFFFFFFFFFF);
 }
 
-TEST(Dec2Hex, Denormalized)
-{
-    double f;
+TEST(Dec2Hex, Zero) {
+  double f;
 
-    EXPECT_EQ(*dp_dec2hex(&(f = -2.225073858507200889e-308)), 0x800FFFFFFFFFFFFF);
-    EXPECT_EQ(*dp_dec2hex(&(f = -4.940656458412465441e-324)), 0x8000000000000001);
-    EXPECT_EQ(*dp_dec2hex(&(f = 4.940656458412465441e-324)), 0x0000000000000001);
-    EXPECT_EQ(*dp_dec2hex(&(f = 2.225073858507200889e-308)), 0x000FFFFFFFFFFFFF);
+  EXPECT_EQ(*dp_dec2hex(&(f = 0)), 0x0000000000000000);
 }
 
-TEST(Dec2Hex, Zero)
-{
-    double f;
-
-    EXPECT_EQ(*dp_dec2hex(&(f = 0)), 0x0000000000000000);
-}
-
-int main()
-{
-    testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
+int main() {
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
 }
--- src/fpc/tests/sp_tests.cc
+++ src/fpc/tests/sp_tests.cc
@@ -1,94 +1,77 @@
+#include <float.h>
 #include <gtest/gtest.h>
 #include <math.h>
 
-extern "C"
-{
+extern "C" {
 #include <fpc.h>
 }
 
-TEST(Hex2Dec, Nan)
-{
-    unsigned n;
+TEST(Hex2Dec, Nan) {
+  unsigned n;
 
-    EXPECT_TRUE(isnan(*sp_hex2dec(&(n = 0xFFFFFFFF))));
-    EXPECT_TRUE(isnan(*sp_hex2dec(&(n = 0xFF800001))));
-    EXPECT_TRUE(isnan(*sp_hex2dec(&(n = 0x7F800001))));
-    EXPECT_TRUE(isnan(*sp_hex2dec(&(n = 0x7FFFFFFF))));
+  EXPECT_TRUE(isnan(*sp_hex2dec(&(n = 0xFFFFFFFF))));
+  EXPECT_TRUE(isnan(*sp_hex2dec(&(n = 0xFF800001))));
+  EXPECT_TRUE(isnan(*sp_hex2dec(&(n = 0x7F800001))));
+  EXPECT_TRUE(isnan(*sp_hex2dec(&(n = 0x7FFFFFFF))));
 }
 
-TEST(Hex2Dec, Inf)
-{
-    unsigned n;
+TEST(Hex2Dec, Inf) {
+  unsigned n;
 
-    EXPECT_TRUE(isinf(*sp_hex2dec(&(n = 0xFF800000))));
-    EXPECT_TRUE(isinf(*sp_hex2dec(&(n = 0x7F800000))));
+  EXPECT_TRUE(isinf(*sp_hex2dec(&(n = 0xFF800000))));
+  EXPECT_TRUE(isinf(*sp_hex2dec(&(n = 0x7F800000))));
 }
 
-TEST(Hex2Dec, Normalized)
-{
-    unsigned n;
+TEST(Hex2Dec, Normalized) {
+  unsigned n;
 
-    EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0xFF7FFFFF)), -3.40282347e+38);
-    EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x80800000)), -1.17549435e-38);
-    EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x00800000)), 1.17549435e-38);
-    EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x7F7FFFFF)), 3.40282347e+38);
+  EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0xFF7FFFFF)), -FLT_MAX);
+  EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x80800000)), -FLT_MIN);
+  EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x00800000)), FLT_MIN);
+  EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x7F7FFFFF)), FLT_MAX);
 }
 
-TEST(Hex2Dec, Denormalized)
-{
-    unsigned n;
+TEST(Hex2Dec, Denormalized) {
+  unsigned n;
 
-    EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x807FFFFF)), -1.17549421e-38);
-    EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x80000001)), -1.40129846e-45);
-    EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x00000001)), 1.40129846e-45);
-    EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x007FFFFF)), 1.17549421e-38);
+  EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x807FFFFF)), -FLT_MIN + FLT_TRUE_MIN);
+  EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x80000001)), -FLT_TRUE_MIN);
+  EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x00000001)), FLT_TRUE_MIN);
+  EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x007FFFFF)), FLT_MIN - FLT_TRUE_MIN);
 }
 
-TEST(Hex2Dec, Zero)
-{
-    unsigned n;
+TEST(Hex2Dec, Zero) {
+  unsigned n;
 
-    EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x80000000)), 0);
-    EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x00000000)), 0);
+  EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x80000000)), 0);
+  EXPECT_FLOAT_EQ(*sp_hex2dec(&(n = 0x00000000)), 0);
 }
 
-TEST(Dec2Hex, Inf)
-{
-    float f;
+TEST(Dec2Hex, Normalized) {
+  float f;
 
-    EXPECT_EQ(*sp_dec2hex(&(f = -3.40282347e+50f)), 0xFF800000);
-    EXPECT_EQ(*sp_dec2hex(&(f = 3.40282347e+50f)), 0x7F800000);
+  EXPECT_EQ(*sp_dec2hex(&(f = -FLT_MAX)), 0xFF7FFFFF);
+  EXPECT_EQ(*sp_dec2hex(&(f = -FLT_MIN)), 0x80800000);
+  EXPECT_EQ(*sp_dec2hex(&(f = FLT_MIN)), 0x00800000);
+  EXPECT_EQ(*sp_dec2hex(&(f = FLT_MAX)), 0x7F7FFFFF);
 }
 
-TEST(Dec2Hex, Normalized)
-{
-    float f;
+TEST(Dec2Hex, Denormalized) {
+  float f;
 
-    EXPECT_EQ(*sp_dec2hex(&(f = -3.40282347e+38f)), 0xFF7FFFFF);
-    EXPECT_EQ(*sp_dec2hex(&(f = -1.17549435e-38f)), 0x80800000);
-    EXPECT_EQ(*sp_dec2hex(&(f = 1.17549435e-38f)), 0x00800000);
-    EXPECT_EQ(*sp_dec2hex(&(f = 3.40282347e+38f)), 0x7F7FFFFF);
+  EXPECT_EQ(*sp_dec2hex(&(f = -FLT_MIN + FLT_TRUE_MIN)), 0x807FFFFF);
+  EXPECT_EQ(*sp_dec2hex(&(f = -FLT_TRUE_MIN)), 0x80000001);
+  EXPECT_EQ(*sp_dec2hex(&(f = FLT_TRUE_MIN)), 0x00000001);
+  EXPECT_EQ(*sp_dec2hex(&(f = FLT_MIN - FLT_TRUE_MIN)), 0x007FFFFF);
 }
 
-TEST(Dec2Hex, Denormalized)
-{
-    float f;
+TEST(Dec2Hex, Zero) {
+  float f;
 
-    EXPECT_EQ(*sp_dec2hex(&(f = -1.17549421e-38f)), 0x807FFFFF);
-    EXPECT_EQ(*sp_dec2hex(&(f = -1.40129846e-45f)), 0x80000001);
-    EXPECT_EQ(*sp_dec2hex(&(f = 1.40129846e-45f)), 0x00000001);
-    EXPECT_EQ(*sp_dec2hex(&(f = 1.17549421e-38f)), 0x007FFFFF);
+  EXPECT_EQ(*sp_dec2hex(&(f = 0)), 0x00000000);
 }
 
-TEST(Dec2Hex, Zero)
-{
-    float f;
-
-    EXPECT_EQ(*sp_dec2hex(&(f = 0)), 0x00000000);
-}
-
-int main()
-{
-    testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
+int main() {
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
 }
